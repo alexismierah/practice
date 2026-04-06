5f4a79029fc4e5225c9ada7676cbe5fd5edecab7
alexismierah <petalcurin.alexis03@gmail.com>
2026-04-06 20:54:12 +0800

    tab logo

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,7 +1,13 @@
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
-
 import "./globals.css"
+
+export const metadata = {
+  title: "Unifix ICT Solutions",
+  icons: {
+    icon: "/Favicon.png",
+  },
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
